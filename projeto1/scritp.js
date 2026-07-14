c473a7332b993e3819f58ef843ff4c3285e36f2b
Renan Jyun <r.jyun10@gmail.com>
2026-07-13 21:55:13 -0300

    Add files via upload

---
+++ projeto1/scritp.js
@@ -0,0 +1,19 @@
+function carregarComponente(idElemento, caminhoArquivo) {
+  fetch(caminhoArquivo)
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById(idElemento).innerHTML = data;
+    })
+    .catch((error) => console.error("Erro ao carregar o componente:", error));
+}
+
+// 1. Identifica onde o site está rodando (Local ou GitHub)
+const nomeRepositorio = window.location.pathname.split("/")[1];
+const emProducao = window.location.hostname.includes("github.io");
+
+// 2. Define a base incluindo a nova pasta 'components'
+const urlBase = emProducao ? `/${nomeRepositorio}/includes/` : "/includes/";
+
+// 3. Executa a chamada juntando a base com os arquivos
+carregarComponente("header-container", urlBase + "header.html");
+carregarComponente("footer-container", urlBase + "footer.html");
